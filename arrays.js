f802336129b6ffe8fc8401e85cc86fd87fef07fb
Sabrie Morales <sabriemorales1995@gmail.com>
2018-06-29 16:42:50 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+function addElementToBeginningOfArray(arr,element){
+  var temp = [element, ...arr];
+  return temp;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr,element){
+  arr =arr.unshift(element);
+  return arr;
+}
+
+function addELementToEndOfArray(arr, element){
+  var temp = [ ...arr,element];
+  return temp;
+}
+
+function destructivelyAddElementToEndOfArray(arr, element){
+  arr =arr.push(element);
+  return arr;
+}
+
+function accessElementInArray(arr, index){
+  return arr[index];
+}
+
+function 
